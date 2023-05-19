e875b755c215388f959a93e515369b98b04b3e20
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-05-19 20:21:14 -0300

    aa

--- lista/lista.js
+++ lista/lista.js
@@ -19,12 +19,13 @@ function adicionar() {
     document.getElementById("Autor").value = '';
     var creater = '<p id="A' + i + '">' + autor + '</p>';
 
-    var button = '<button id="bt' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
+    var button1 = '<button id="bt1' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
+    var button2 = '<button id="bt2' + i + '" onclick="editar(' + i + ')">Editar</button>';
 
     var div = document.createElement("div");
     div.setAttribute("id", "div" + i);
 
-    var tudo = title + date + descri + creater + button;
+    var tudo = title + date + descri + creater + button1 + button2;
 
     document.body.innerHTML += '<div id="div' + i + '">' + tudo + '</div>';
 }
@@ -32,4 +33,37 @@ function adicionar() {
 function desfazer(id) {
     var divs = document.getElementById("div" + id);
     divs.hidden = true;
+}
+
+function editar(id) {
+    var divs = document.getElementById("div" + id);
+    divs.hidden = true;
+    
+    i++;
+
+    var titulo = document.getElementById("Titulo").value;
+    document.getElementById("Titulo").value = '';
+    var title = '<p id="T' + i + '">' + titulo + '</p>';
+
+    var data = document.getElementById("Data").value;
+    document.getElementById("Data").value = '';
+    var date = '<p id="D' + i + '">' + data + '</p>';
+
+    var des = document.getElementById("Descricao").value;
+    document.getElementById("Descricao").value = '';
+    var descri = '<p id="DD' + i + '">' + des + '</p>';
+
+    var autor = document.getElementById("Autor").value;
+    document.getElementById("Autor").value = '';
+    var creater = '<p id="A' + i + '">' + autor + '</p>';
+
+    var button1 = '<button id="bt1' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
+    var button2 = '<button id="bt2' + i + '" onclick="editar(' + i + ')">Editar</button>';
+
+    var div = document.createElement("div");
+    div.setAttribute("id", "div" + i);
+
+    var tudo = title + date + descri + creater + button1 + button2;
+
+    document.body.innerHTML += '<div id="div' + i + '">' + tudo + '</div>';
 }
