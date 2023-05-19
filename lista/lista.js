37eab992a9d1ad35f40d86c4f511463f803a92e7
MatheusScaranello <matheusscaranello.564@gmail.com>
2023-05-19 20:05:00 -0300

    aa

--- lista/lista.js
+++ lista/lista.js
@@ -1,84 +1,35 @@
-/* var i = 0;
+var i = 0;
+
 function adicionar() {
-    i++
+    i++;
 
     var titulo = document.getElementById("Titulo").value;
     document.getElementById("Titulo").value = '';
-    document.getElementById("T" + i).innerHTML = titulo;
+    var title = '<p id="T' + i + '">' + titulo + '</p>';
+
     var data = document.getElementById("Data").value;
     document.getElementById("Data").value = '';
-    document.getElementById("D" + i).innerHTML = data;
+    var date = '<p id="D' + i + '">' + data + '</p>';
+
     var des = document.getElementById("Descricao").value;
     document.getElementById("Descricao").value = '';
-    document.getElementById("DD" + i).innerHTML = des;
+    var descri = '<p id="DD' + i + '">' + des + '</p>';
+
     var autor = document.getElementById("Autor").value;
     document.getElementById("Autor").value = '';
-    document.getElementById("A" + i).innerHTML = autor;
-    document.getElementById("bt" + i).hidden = false;
+    var creater = '<p id="A' + i + '">' + autor + '</p>';
+
+    var button = '<button id="bt' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
+
+    var div = document.createElement("div");
+    div.setAttribute("id", "div" + i);
+
+    var tudo = title + date + descri + creater + button;
+
+    document.body.innerHTML += '<div id="div' + i + '">' + tudo + '</div>';
 }
 
-function remover() {
-    if (i > 20) {
-        var menos = i - 1;
-        i = menos;
-    }
-
-    document.getElementById("T" + i).innerHTML = "";
-    document.getElementById("D" + i).innerHTML = "";
-    document.getElementById("DD" + i).innerHTML = "";
-    document.getElementById("A" + i).innerHTML = "";
-    document.getElementById("bt" + i).hidden = true;
-    var menos = i - 1;
-    i = menos;
-    }*/
-
-    var i = 0;
-
-    function adicionar() {
-        i++;
-    
-        var titulo = document.getElementById("Titulo").value;
-        document.getElementById("Titulo").value = '';
-        var title = document.createElement("p");
-        title.setAttribute("id", "T" + i);
-        title.innerHTML = titulo;
-    
-        var data = document.getElementById("Data").value;
-        document.getElementById("Data").value = '';
-        var date = document.createElement("p");
-        date.setAttribute("id", "D" + i);
-        date.innerHTML = data;
-    
-        var des = document.getElementById("Descricao").value;
-        document.getElementById("Descricao").value = '';
-        var descri = document.createElement("p");
-        descri.setAttribute("id", "DD" + i);
-        descri.innerHTML = des;
-    
-        var autor = document.getElementById("Autor").value;
-        document.getElementById("Autor").value = '';
-        var creater = document.createElement("p");
-        creater.setAttribute("id", "A" + i);
-        creater.innerHTML = autor;
-    
-        var button = document.createElement("button");
-        button.innerHTML = "Desfazer";
-        button.setAttribute("id", "bt" + i);
-        button.setAttribute("onclick", "desfazer(" + i + ")");
-    
-        var div = document.createElement("div");
-        div.setAttribute("id", "div" + i);
-    
-        div.appendChild(title);
-        div.appendChild(date);
-        div.appendChild(descri);
-        div.appendChild(creater);
-        div.appendChild(button);
-    
-        document.body.appendChild(div);
-    }
-    
-    function desfazer(id) {
-        var element = document.getElementById("div" + id);
-        element.hidden = true;
-    }
+function desfazer(id) {
+    var divs = document.getElementById("div" + id);
+    divs.hidden = true;
+}
