7ef19bc5cf70cad7a2e15b719086adda81b08061
Giulia Marques <giuliamarques.564@gmail.com>
2023-06-02 13:43:37 -0300

    commit

--- lista/lista.js
+++ lista/lista.js
@@ -6,19 +6,19 @@ var i = 0;
     
         var titulo = document.getElementById("Titulo").value;
         document.getElementById("Titulo").value = '';
-        var title = '<p id="T' + i + '">' + titulo + '</p>';
+        var title = '<p id="T' + i + '" class="paragrafos">' + titulo + '</p>';
     
         var data = document.getElementById("Data").value;
         document.getElementById("Data").value = '';
-        var date = '<p id="D' + i + '">' + data + '</p>';
+        var date = '<p id="D' + i + '" class="paragrafos">' + data + '</p>';
     
         var des = document.getElementById("Descricao").value;
         document.getElementById("Descricao").value = '';
-        var descri = '<p id="DD' + i + '">' + des + '</p>';
+        var descri = '<p id="DD' + i + '" class="paragrafos">' + des + '</p>';
     
         var autor = document.getElementById("Autor").value;
         document.getElementById("Autor").value = '';
-        var creater = '<p id="A' + i + '">' + autor + '</p>';
+        var creater = '<p id="A' + i + '" class="paragrafos">' + autor + '</p>';
     
         var button1 = '<button class="edit_desf" id="bt1' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
         var button2 = '<button class="edit_desf" id="bt2' + i + '" onclick="editar(' + i + ')">Editar</button>';
@@ -39,27 +39,27 @@ var i = 0;
 
 
 function editar(id) {
-i++
+
     var divs = document.getElementById("div" + id);
         
     var titulo = document.getElementById("Titulo").value;
     document.getElementById("Titulo").value = '';
-    var title = '<p id="T' + i + '">' + titulo + '</p>';
+    var title = '<p id="T' + i + '" class="paragrafos">' + titulo + '</p>';
 
     var data = document.getElementById("Data").value;
     document.getElementById("Data").value = '';
-    var date = '<p id="D' + i + '">' + data + '</p>';
+    var date = '<p id="D' + i + '" class="paragrafos">' + data + '</p>';
 
     var des = document.getElementById("Descricao").value;
     document.getElementById("Descricao").value = '';
-    var descri = '<p id="DD' + i + '">' + des + '</p>';
+    var descri = '<p id="DD' + i + '" class="paragrafos">' + des + '</p>';
 
     var autor = document.getElementById("Autor").value;
     document.getElementById("Autor").value = '';
-    var creater = '<p id="A' + i + '">' + autor + '</p>';
+    var creater = '<p id="A' + i + '" class="paragrafos">' + autor + '</p>';
 
-    var button1 = '<button id="bt1' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
-    var button2 = '<button id="bt2' + i + '" onclick="editar(' + i + ')">Editar</button>';
+    var button1 = '<button class="edit_desf" id="bt1' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
+    var button2 = '<button class="edit_desf" id="bt2' + i + '" onclick="editar(' + i + ')">Editar</button>';
 
     var tudo = title + date + descri + creater + button1 + button2;
 
