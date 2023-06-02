8c204060f7d0613e065eccf1c4caa3694d18fdfa
Giulia Marques <giuliamarques.564@gmail.com>
2023-06-02 08:28:07 -0300

    aaa

--- lista/lista.js
+++ lista/lista.js
@@ -1,33 +1,39 @@
 var i = 0;
+
 function adicionar() {
     i++;
+
     var titulo = document.getElementById("Titulo").value;
     document.getElementById("Titulo").value = '';
     var title = '<p id="T' + i + '">' + titulo + '</p>';
+
     var data = document.getElementById("Data").value;
     document.getElementById("Data").value = '';
     var date = '<p id="D' + i + '">' + data + '</p>';
+
     var des = document.getElementById("Descricao").value;
     document.getElementById("Descricao").value = '';
     var descri = '<p id="DD' + i + '">' + des + '</p>';
+
     var autor = document.getElementById("Autor").value;
     document.getElementById("Autor").value = '';
     var creater = '<p id="A' + i + '">' + autor + '</p>';
+
     var button1 = '<button class="edit_desf" id="bt1' + i + '" onclick="desfazer(' + i + ')">Desfazer</button>';
     var button2 = '<button class="edit_desf" id="bt2' + i + '" onclick="editar(' + i + ')">Editar</button>';
+
     var tudo = title + date + descri + creater + button1 + button2;
+
     var div = '<div class="noticia_add" id="div' + i + '">' + tudo + '</div>';
 
-    
-    var divdasdivs = document.getElementById("divdasdivs");
-    divdasdivs.innerHTML += div;
+    body.innerHTML = div;
 }
 
-
 function editar(id) {
-        var divs = document.getElementById("div" + id);
+
+    var divs = document.getElementById("div" + id);
         
-        var titulo = document.getElementById("Titulo").value;
+    var titulo = document.getElementById("Titulo").value;
     document.getElementById("Titulo").value = '';
     var title = '<p id="T' + id + '">' + titulo + '</p>';
 
@@ -57,4 +63,5 @@ function editar(id) {
 
 function desfazer(id) {
     var divs = document.getElementById("div" + id);
+    divs.hidden = true;
 }
