1c45e78474faf40aba6b116ef41c005189bc8849
Andrey Botero <andreybotero564@gmail.com>
2023-06-16 09:04:30 -0300

    new commit

--- layouts/header2.js
+++ layouts/header2.js
@@ -1,35 +1,29 @@
-var header           = document.getElementById('header');
+var header = document.getElementById('header');
 var navigationHeader = document.getElementById('navigation_header');
-var showSidebar      = false;
+var showSidebar = false;
 
-function toggleSidebar()
-{
-    showSidebar = !showSidebar;
-    if(showSidebar)
-    {
-        navigationHeader.style.marginLeft = '-10vw'
-        navigationHeader.style.animationName = 'showSidebar';
-    }
-    else
-    {
-        navigationHeader.style.marginLeft = '-100vw';
-        navigationHeader.style.animationName = '';
-    }
+function toggleSidebar() {
+  showSidebar = !showSidebar;
+  if (showSidebar) {
+    navigationHeader.style.marginLeft = '-10vw';
+    navigationHeader.style.animationName = 'showSidebar';
+  } else {
+    navigationHeader.style.marginLeft = '-100vw';
+    navigationHeader.style.animationName = 'showSidebar';
+  }
 }
 
-function closeSidebar()
-{
-    if(showSidebar)
-    {
-        showSidebar = true;
-        toggleSidebar();
-    }
+function closeSidebar() {
+  if (showSidebar) {
+    showSidebar = false; // Corrigido: alterado para false
+    toggleSidebar();
+  }
 }
 
-window.addEventListener('resize', function(event) {
-    if(window.innerWidth > 768 && showSidebar) 
-    {  
-        showSidebar = true;
-        toggleSidebar();
-    }
+window.addEventListener('resize', function (event) {
+  if (window.innerWidth > 768 && showSidebar) {
+    showSidebar = false; // Corrigido: alterado para false
+    toggleSidebar();
+  }
 });
+    
